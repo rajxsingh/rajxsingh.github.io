8a627c38d1db9c301fda0cf2d43c9aa6fed8df9c
Rajbir Singh <rajbir.singh@brunata-muenchen.de>
2025-01-22 13:44:58 +0100

    update BA

--- ba/assets/slidev/DrawingPreview-j7EEO8Jf.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-sVBBpi_3.js";import"../modules/vue-C7M9jA-P.js";import"../index-D9ZsvOx3.js";import"../modules/shiki--7Ahuh23.js";export{o as default};
